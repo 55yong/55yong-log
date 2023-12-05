5cd4604088ebb6be8522333f280f1b71cc5a2bf0
Yongjun OH <132319467+55yong@users.noreply.github.com>
2023-12-05 17:23:35 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -15,10 +15,6 @@ const CONFIG = {
       name: `IT's 대나무숲`,
       href: "https://padlet.com/a56986631/it-s-seistm8ti1tlux4i",
     },
-    {
-      name: `동아리 신청서 작성`,
-      href: "https://forms.gle/VrLTiV9kbxYjtYyY9",
-    },
   ],
   // blog setting (required)
   blog: {
